2a6a4ced3eda99c0756a1839a6e73a9b8d4f61c1
8bitty <max@koeninger.org>
2023-01-04 15:07:47 -0600

    functional login

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import random
+import time
+import sys
 
+def setup():
+    ans=input('do you want to login or register? ')
+    if(ans=='register'):
+        username = str(input('username: '))
+        password = str(input('password: '))
+        login = (username+password)
+        with open('usernames.txt', 'w') as f:
+            f.write(login)
+    elif(ans=='login'):
+        username = str(input('username: '))
+        password = str(input('password: '))
+        login = (username+password)
+        with open('usernames.txt', 'r') as f:
+            for line in f:
+                if line == login:
+                    print('you have logged in')
+                    a = True
+                    break
+                else:
+                    a = False
+                    pass
+            if(a==False):
+                print('your username and/or password was incorrect')
+                setup()
+    else:
+        print('please either input "login" or "register"')
+        setup()
+setup()
