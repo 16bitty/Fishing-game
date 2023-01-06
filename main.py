0023069dc1a38db2edc70eb91495c648391b4733
8bitty <max@koeninger.org>
2023-01-06 11:59:19 -0600

    fully functional rod & casting system

--- main.py
+++ main.py
@@ -46,17 +46,25 @@ def setup():
 print(name)
 
 rod = 'basic'
-flist=['salmon', 'trout', 'catfish', 'eel', 'kraken']
-fcommon = ['salmon', 'trout', 'carp', 'tuna', 'boot']
-frare = ['eel', 'lobster', 'angelfish', 'squid', 'octopus']
-flegendary = ['kraken', 'dragon', 'leviathan']
+common = ['salmon', 'trout', 'carp', 'tuna', 'boot']
+rare = ['eel', 'lobster', 'angelfish', 'squid']
+legendary = ['kraken', 'dragon', 'leviathan']
+rarity=[common,rare,legendary]
 money = 0
 
 #this randomly selects whether to choose from fcommon,frare,or flegendary
 #once chosen, it chooses a random item and appends it to fish.txt
 def cast():
-    fish = random.choice(flist)
-    print('you caught a ' + fish)
+    global rarity
+    if(rod=='basic'):
+        frarity=random.choices(rarity, weights=[90,10,0])
+    elif(rod=='reinforced'):
+        frarity=random.choices(rarity, weights=[49,50,1])
+    elif(rod=='platinum'):
+        frarity=random.choices(rarity, weights=[10,80,10])
+    fish=random.choice(frarity)
+    fish=random.choice(fish)
+    print('you caught a '+fish)
     with open('fish.txt','a') as f:
         f.write(fish+'\n')
 
@@ -66,24 +74,24 @@ def sell():
     with open('fish.txt','r') as f:
         tmoney=0
         for line in f:
-            fish = line
-            if(fish=='salmon\n'):
+            fish = line.strip()
+            if(fish=='salmon' or fish=='trout'):
                 money+= 3
                 tmoney+= 3
-            elif(fish=='trout\n'):
+            elif(fish=='carp' or fish=='tuna'):
                 money+= 5
                 tmoney+= 5
-            elif(fish=='catfish\n'):
+            elif(fish=='eel' or fish=='lobster'):
                 money+= 10
                 tmoney+= 10
-            elif(fish=='eel\n'):
+            elif(fish=='angelfish' or fish=='squid'):
                 money+= 15
                 tmoney+= 15
-            elif(fish=='kraken\n'):
-                money+= 50
-                tmoney+= 50
+            elif(fish=='kraken' or fish=='dragon' or fish=='leviathan'):
+                money+= 200
+                tmoney+= 200
             else:
-                print('error')
+                pass
         print('you sold all your fish and earned $'+str(tmoney)+'\nyour curent money is $'+str(money))
     with open('fish.txt','w') as f:
         f.write("")
@@ -92,17 +100,17 @@ def buy():
     global rod
     global money
     print('there are three rods available for purchase, your current rod is: '+rod)
-    print('basic - $20\nreinforced - $200\nplatinum - $1000')
+    print('basic - $20\nreinforced - $100\nplatinum - $1000')
     print('your current money is: $'+str(money))
     choice=input('which rod do you want to buy? enter "none" to exit: ')
-    if(choice=='basic' and money<20 or choice=='reinforced' and money<200 or choice=='platinum' and money<1000):
+    if(choice=='basic' and money<20 or choice=='reinforced' and money<100 or choice=='platinum' and money<1000):
         print("you don't have enough money for that")
     elif(choice=='basic'):
         money-=20
         rod='basic'
         print("your new rod is: "+rod)
     elif(choice=='reinforced'):
-        money-=200
+        money-=100
         rod='reinforced'
         print("your new rod is: "+rod)
     elif(choice=='platinum'):
